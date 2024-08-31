bb69b84e4ee3742d632ca6ce7255c24ee035309e
Alexander Chehovskii <let.value@gmail.com>
2024-09-01 00:04:39 +0500

    Initial

---
+++ window/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+
+namespace window;
+
+public partial class App : Application {
+    private readonly Lazy<MainWindow> MainWindow;
+
+    public App(Lazy<MainWindow> mainWindow) {
+        this.MainWindow = mainWindow;
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args) {
+        MainWindow.Value.Activate();
+    }
+}
---
+++ window/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using window;
+
+var host = new HostBuilder()
+    .ConfigureServices(services => services
+        .AddLogging(logging => logging.AddConsole().AddDebug())
+        .AddSingleton<App>()
+        .AddSingleton<MainWindow>()
+        .AddSingleton(HostingServiceExtensions.AddLazy<App>)
+        .AddSingleton(HostingServiceExtensions.AddLazy<MainWindow>)
+        .AddHostedService<WinUiService<App>>())
+    .Build();
+
+await host.RunAsync();
